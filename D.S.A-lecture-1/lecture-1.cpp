2410a468e12c6b0bd948b37b39fa9779586bc7a1
DEEPAK-SINGH-GOOR <deepaksingh745@gmail.com>
2024-02-02 11:11:57 +0530

    D.S.A-lecture-1

---
+++ D.S.A-lecture-1/lecture-1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a,b;
+    cout<<"Addition"<<endl;
+    cout<<"Enter A:";
+    cin >>a;
+    cout<<"Enter B:";
+    cin >>b;
+    int c1 = a + b;
+    cout<<endl <<"Subtration"<<endl <<endl;
+    cout<<"Enter A:";
+    cin>>a;
+    cout<<"Enter B:";
+    cin>>b;
+    int c2 =a-b;
+    cout<<endl <<"Multiplication"<<endl <<endl;
+    cout<<"Enter A:";
+    cin>>a;
+    cout<<"Enter B:";
+    cin>>b;
+    int c3=a*b;
+    cout<<endl <<"Division"<<endl <<endl;
+    cout<<"Enter A:";
+    cin>>a;
+    cout<<"Enter B:";
+    cin>>b;
+    int c4=a/b;
+    cout <<endl <<"Percentage"<<endl <<endl;
+    cout<<"Enter A:";
+    cin>>a;
+    cout<<"Enter B:";
+    cin>>b;
+    int c5=(a*b)/100;
+    
+    
+    
+    cout << "The (+) of A and B is: " << c1 << endl;
+    cout << "The (-) of A and B is:" <<c2 <<endl;
+    
+    cout << "The (*) of A and B is: " << c3 << endl;
+    cout << "The (/) of A and B is:" <<c4 <<endl;
+    
+    cout << "The (%) of A and B is: " << c5 << endl;
+}
+
